cc35e62fad9c51d36f67faecb4a57ddd3ae935f5
owalaye <abimbolaowalaye@gmail.com>
2023-02-18 21:21:48 +1300

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include<stdio.h>
-int main() {
-	           puts(""Programming is like building a multilingual puzzle");
-
-		              return 0;
+#include <stdio.h>
+/**
+ * writing a program using put function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
