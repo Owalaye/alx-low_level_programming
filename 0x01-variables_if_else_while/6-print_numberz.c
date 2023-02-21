d1e5bd7dbfbb25e1d03930d4d31b4e4787e9ab8a
owalaye <abimbolaowalaye@gmail.com>
2023-02-21 03:58:00 +0100

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - base num
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	printf(i);
+	putchar("\n");
+
+	return (0);
+}
