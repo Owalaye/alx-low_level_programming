5e3401a4b40da5f085f48d893697b3c1c9d6aa8e
owalaye <abimbolaowalaye@gmail.com>
2023-02-21 04:07:40 +0100

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - upper case
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char small;
+
+	for (small = 'z'; small <= 'a'; small++)
+	putchar(small);
+	putchar('\n');
+
+	return (0);
+}
