1eb5c784aa6e4a045c89055cbc879b25817a6423
owalaye <abimbolaowalaye@gmail.com>
2023-02-22 21:54:01 +0100

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-	_ptchar('_');
-	_ptchar('p');
-	_ptchar('u');
-	_ptchar('t');
-	_ptchar('c');
-	_ptchar('h');
-	_ptchar('a');
-	_ptchar('r');
-	_ptchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 
 }
