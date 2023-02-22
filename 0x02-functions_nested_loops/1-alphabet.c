b6caa89078142dc00e92157e147ecc575ff85a27
owalaye <abimbolaowalaye@gmail.com>
2023-02-22 22:23:26 +0100

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print_alphabet
+ * Return: Always (success)
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
