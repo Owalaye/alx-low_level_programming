e55a4704580ff206ff5c75abb6710de8ad92f541
owalaye <abimbolaowalaye@gmail.com>
2023-02-22 23:29:22 +0100

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - alphabet
+ * @c: C character
+ * Return: Always (success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
